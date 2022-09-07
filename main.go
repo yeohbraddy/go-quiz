66cb37fa58bc1b0eb1237d771b0b179ccfeb4614
Braddy Yeoh <braddy.yeoh@ucdconnect.ie>
2022-09-07 22:02:35 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	csvFilename := flag.String("csv", "data.csv", "a csv file in the format of 'question, answer'")
+	timeLimit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
+	flag.Parse()
+
+	f, err := os.Open(*csvFilename)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	lines, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	problems := parseLines(lines)
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+	correct := 0
+problemloop:
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = ", i+1, p.q)
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Println()
+			break problemloop
+		case answer := <-answerCh:
+			if answer == p.a {
+				correct++
+			}
+		}
+	}
+
+	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
+}
+
+type problem struct {
+	q string
+	a string
+}
